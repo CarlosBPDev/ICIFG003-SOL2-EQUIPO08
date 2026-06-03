1b3215f3b97ca015763e6f80f82b17d0a6a7aa99
nbarradev <ignacio.barrach@gmail.com>
2026-06-03 09:52:28 -0400

    Integración de angular

---
+++ Frontend/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', loadComponent: () => import('./features/auth/pages/login.component').then(m => m.LoginComponent) },
+  { path: 'register', loadComponent: () => import('./features/auth/pages/register.component').then(m => m.RegisterComponent) },
+  { path: 'salas', loadComponent: () => import('./features/salas/pages/salas.component').then(m => m.SalasComponent) },
+  { path: 'reservas', loadComponent: () => import('./features/reservas/pages/reservas.component').then(m => m.ReservasComponent) },
+  { path: 'estudiantes', loadComponent: () => import('./features/estudiantes/pages/estudiantes.component').then(m => m.EstudiantesComponent) },
+  { path: '**', redirectTo: 'login' }
+];
